3adefdf537aa6bb3392361ac505a564c90166625
madela2 <madeleine02@hotmail.no>
2026-04-13 13:12:57 +0200

    Remove scroll-to-top button and associated JavaScript functionality; clean up styles

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
-// Scroll to top button
-let topbutton = document.getElementById("topBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { 
-    scrollFunction() 
-};
-
-function scrollFunction() {
-    if (!topbutton) return;
-
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        topbutton.style.display = "flex";
-    } else {
-        topbutton.style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 function scrollToProjects() {
     const targetSection = document.getElementById("projects");
-    if (targetSection) {
-        targetSection.scrollIntoView({behavior: "smooth"});
-    }
-}
-
-function scrollToAboutMe() {
-    const targetSection = document.getElementById("about-me");
     if (targetSection) {
         targetSection.scrollIntoView({behavior: "smooth"});
     }
